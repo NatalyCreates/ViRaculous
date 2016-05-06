62685282946861cd2c5b57e7fc666d97c4f6f228
stavhertz@gmail.com <stavhertz@gmail.com>
2016-05-07 01:57:12 +0300

    Adding text controls and script to update timer and later on also score

---
+++ Assets/Scripts/TextUpdater.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextUpdater : MonoBehaviour {
+
+    public Text TimerText;
+    public Text ScoreText;
+
+    public static int Score = 0;
+
+    float timeLeft = 300.0f;
+	// Use this for initialization
+	void Start () {
+        SetScore();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        TimerText.text = "Time Left: " + Mathf.Round(timeLeft);
+        if (Mathf.Round(timeLeft) == 0)
+        {
+            //Code to show final score
+        }
+
+	}
+
+    void SetScore() {
+        ScoreText.text = "Score: " + Score;
+    }
+}
