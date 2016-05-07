f607c4119cc30a46663948530b14fd322cb8aff1
stavhertz@gmail.com <stavhertz@gmail.com>
2016-05-07 12:24:14 +0300

    Adding pause and restart game functionality

--- Assets/Scripts/TextUpdater.cs
+++ Assets/Scripts/TextUpdater.cs
@@ -10,28 +10,59 @@ public class TextUpdater : MonoBehaviour {
 
     public static int Score = 0;
 
-    float timeLeft = 300.0f;
-    private bool timerEnded = false;
+    float initTimeVal = 300.0f;
+    float timeLeft;
+    private bool gamePaused = false;
 	// Use this for initialization
 	void Start () {
+        timeLeft = initTimeVal;
         SetScore();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+    void Update()
+    {
+
         if (Mathf.Round(timeLeft) > 0)
         {
-            timeLeft -= Time.deltaTime;            
+            timeLeft -= Time.deltaTime;
+            SetScore();
         }
         else //if (Mathf.Round(timeLeft) == 0)
         {
             ScoreText.text = "";
             FinalScore.text = "Final score: " + Score;
+            Time.timeScale = 0;
         }
 
-        SetScore();
         TimerText.text = "Time Left: " + Mathf.Round(timeLeft);
-	}
+
+        if (Input.GetButtonUp("Tap"))
+        {
+            if (Mathf.Round(timeLeft) == 0)
+            {
+                timeLeft = initTimeVal;
+                Time.timeScale = 1;
+                Score = 0;
+                TimerText.text = "";
+                FinalScore.text = "";
+                gamePaused = false;
+                SetScore();
+            }
+            else if (gamePaused) //Resume
+            {
+                FinalScore.text = "";
+                Time.timeScale = 1;
+                gamePaused = false;
+            }
+            else //Pause
+            {
+                FinalScore.text = "Game Paused";
+                Time.timeScale = 0;
+                gamePaused = true;
+            }
+        }
+    }
 
     void SetScore() {
         ScoreText.text = "Score: " + Score;
